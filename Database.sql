9452b2ba4e161353aeded17c3798a970380d670d
Kristoffer Sakaria <kristoffer.sakaria@gmail.com>
2016-04-22 11:07:02 +0200

    Added SQL Script for generating tables and relations.

---
+++ Database.sql
@@ -0,0 +1,166 @@
+﻿
+/****** Object:  Table [dbo].[Client]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Client](
+	[Id] [nvarchar](128) NOT NULL,
+	[Secret] [nvarchar](128) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[ApplicationType] [int] NOT NULL,
+	[Active] [bit] NOT NULL,
+	[RefreshTokenLifeTime] [int] NOT NULL,
+	[AllowedOrigin] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Client] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Member]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Member](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEndDateUtc] [datetime] NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+	[UserName] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.Member] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[MemberClaim]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MemberClaim](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MemberId] [int] NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.MemberClaim] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[MemberLogin]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MemberLogin](
+	[LoginProvider] [nvarchar](128) NOT NULL,
+	[ProviderKey] [nvarchar](128) NOT NULL,
+	[MemberId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.MemberLogin] PRIMARY KEY CLUSTERED 
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC,
+	[MemberId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[MemberRole]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[MemberRole](
+	[MemberId] [int] NOT NULL,
+	[RoleId] [int] NOT NULL,
+ CONSTRAINT [PK_dbo.MemberRoles] PRIMARY KEY CLUSTERED 
+(
+	[MemberId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[RefreshTokens]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RefreshTokens](
+	[Id] [nvarchar](128) NOT NULL,
+	[Subject] [nvarchar](50) NOT NULL,
+	[ClientId] [nvarchar](128) NOT NULL,
+	[IssuedUtc] [datetime] NOT NULL,
+	[ExpiresUtc] [datetime] NOT NULL,
+	[ProtectedTicket] [nvarchar](4000) NOT NULL,
+	[MemberId] [int] NOT NULL,
+ CONSTRAINT [PK_RefreshTokens] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Role]    Script Date: 2016-04-22 11:05:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Role](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](256) NOT NULL,
+ CONSTRAINT [PK_dbo.Role] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[MemberClaim]  WITH CHECK ADD  CONSTRAINT [FK_dbo.MemberClaim_dbo.Member_MemberId] FOREIGN KEY([MemberId])
+REFERENCES [dbo].[Member] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[MemberClaim] CHECK CONSTRAINT [FK_dbo.MemberClaim_dbo.Member_MemberId]
+GO
+ALTER TABLE [dbo].[MemberLogin]  WITH CHECK ADD  CONSTRAINT [FK_dbo.MemberLogin_dbo.Member_MemberId] FOREIGN KEY([MemberId])
+REFERENCES [dbo].[Member] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[MemberLogin] CHECK CONSTRAINT [FK_dbo.MemberLogin_dbo.Member_MemberId]
+GO
+ALTER TABLE [dbo].[MemberRole]  WITH CHECK ADD  CONSTRAINT [FK_dbo.MemberRoles_dbo.Member_MemberId] FOREIGN KEY([MemberId])
+REFERENCES [dbo].[Member] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[MemberRole] CHECK CONSTRAINT [FK_dbo.MemberRoles_dbo.Member_MemberId]
+GO
+ALTER TABLE [dbo].[MemberRole]  WITH CHECK ADD  CONSTRAINT [FK_dbo.MemberRoles_dbo.Roles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Role] ([Id])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[MemberRole] CHECK CONSTRAINT [FK_dbo.MemberRoles_dbo.Roles_RoleId]
+GO
+ALTER TABLE [dbo].[RefreshTokens]  WITH CHECK ADD  CONSTRAINT [FK_RefreshTokens_Client] FOREIGN KEY([ClientId])
+REFERENCES [dbo].[Client] ([Id])
+GO
+ALTER TABLE [dbo].[RefreshTokens] CHECK CONSTRAINT [FK_RefreshTokens_Client]
+GO
+ALTER TABLE [dbo].[RefreshTokens]  WITH CHECK ADD  CONSTRAINT [FK_RefreshTokens_Member] FOREIGN KEY([MemberId])
+REFERENCES [dbo].[Member] ([Id])
+GO
+ALTER TABLE [dbo].[RefreshTokens] CHECK CONSTRAINT [FK_RefreshTokens_Member]
+GO
